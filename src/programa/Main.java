cfd9a61a232e915a4d40854e7afc64a348da0617
Jos3s <karatedkgt17@gmail.com>
2020-10-11 14:05:55 -0300

    Adição/Main do programa

--- src/programa/Main.java
+++ src/programa/Main.java
@@ -1,10 +1,14 @@
 package programa;
 
+import tela.JanelaTabela;
+
 public class Main {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		
-
+		JanelaTabela jt=new JanelaTabela();
+		
 	}
 
 }
